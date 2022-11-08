448ccda10d937f508d7f5d61f5fb3289aa68b097
Min <solunar@vip.qq.com>
2022-11-08 16:52:35 +0800

    Update shop-1.vue

--- src/views/shop-1.vue
+++ src/views/shop-1.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex">
     <ui-border class="col">
-      <ui-item>装备店/道具店</ui-item>
+      <ui-item>装备店/道具店/补给店/修理店</ui-item>
     </ui-border>
     <ui-border class="w-5">
       <ui-item class="text-right">{{ Math.floor(Math.random() * 12345678) }}G</ui-item>
@@ -12,7 +12,10 @@
       <ui-border>
         <ui-item @click="scene = 'buy'" clickable>购买装备</ui-item>
         <ui-item @click="scene = 'sell'" clickable>售卖道具</ui-item>
-        <ui-item @click="(scene = null), (command = null)" clickable>离开</ui-item>
+        <ui-item @click="scene = null" clickable>全部补满</ui-item>
+        <ui-item @click="scene = 'supply'" clickable>部分补给</ui-item>
+        <ui-item @click="scene = 'repair'" clickable>修理</ui-item>
+        <ui-item @click="scene = null" clickable>离开</ui-item>
       </ui-border>
       <ui-border v-if="scene === 'buy' || scene === 'sell'">
         <ui-item v-for="a in actors" v-text="a" :key="a" class="flex justify-between" clickable />
@@ -49,6 +52,25 @@
           <ui-item></ui-item>
         </ui-border>
       </template>
+      <ui-border v-if="scene === 'repair'" class="col">
+        <ui-item @click="command = 'confirm'" class="flex" clickable>
+          <div class="w-3">1号战车</div>
+          <div class="w-5">220mm盖亚炮</div>
+          <div class="w-1">破损</div>
+          <div class="col text-right">{{ Math.floor(Math.random() * 1234) }}G</div>
+        </ui-item>
+        <ui-item @click="command = 'confirm'" class="flex" clickable>
+          <div class="w-3">1号战车</div>
+          <div class="w-5">索罗门2</div>
+          <div class="w-1">大破</div>
+          <div class="col text-right">{{ Math.floor(Math.random() * 1234) }}G</div>
+        </ui-item>
+      </ui-border>
+      <ui-border v-if="scene === 'supply'" class="col">
+        <ui-item @click="command = 'confirm'" class="flex justify-between" clickable><span>装甲片</span><span>1234/9999</span></ui-item>
+        <ui-item @click="command = 'confirm'" class="flex justify-between" clickable><span>220mm盖亚炮</span><span>12/62</span></ui-item>
+        <ui-item @click="command = 'confirm'" class="flex justify-between" clickable><span>S-龙卷风</span><span>9/16</span></ui-item>
+      </ui-border>
     </div>
   </div>
   <ui-border v-if="!scene">
