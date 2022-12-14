458297ed9f5d0e792970ccc75d55e0f4bbe482f1
660e <solunar@vip.qq.com>
2022-12-14 17:42:58 +0800

    Update

--- src/components/q-window.vue
+++ src/components/q-window.vue
@@ -20,6 +20,7 @@ export default {
 <style lang="scss" scoped>
 .q-window {
   background-color: $dark;
+  backdrop-filter: blur($gap);
   border: $border solid;
   box-sizing: border-box;
   padding: $padding $text;
--- src/views/menu/item.vue
+++ src/views/menu/item.vue
@@ -1,36 +1,53 @@
 <template>
   <q-window class="w-500 gap-x">
     <br />
-    <q-text>
-      {{
-        (() => {
-          switch ($store.getters.active) {
-            case 'item':
-              return '道具';
-            case 'important':
-              return '重要道具';
-            case 'tank':
-              return '战车名';
-          }
-        })()
-      }}
-    </q-text>
-    <q-banner :th="['名称', $store.getters.active === 'tank' ? '重量' : '持有数']" />
-    <q-grid :grid="[10, 1]">
-      <q-text v-show="$store.getters.active === 'item'" v-for="i in $d.items[0]" :key="i" between hoverable icon>
-        <span>{{ i }}</span>
-        <span>{{ $r(1, 10) }}</span>
-      </q-text>
-      <q-text v-show="$store.getters.active === 'important'" between hoverable icon>
-        <span>万能钥匙</span>
-        <span>1</span>
-      </q-text>
-      <q-text v-show="$store.getters.active === 'tank'" v-for="e in $d.equipments[1]" :key="e" between hoverable icon>
-        <span>{{ e }}</span>
-        <span>{{ $r(5, 15) }}.00t</span>
-      </q-text>
-    </q-grid>
-    <q-banner :th="[`${$store.getters.active === 'tank' ? '装备' : '道具'}说明`]" />
+
+    <!-- 道具 -->
+    <template v-if="$store.getters.active === 'item'">
+      <q-text>道具</q-text>
+      <q-banner :th="['名称', '持有数']" />
+      <q-grid :grid="[10, 1]">
+        <q-text v-show="$store.getters.active === 'item'" v-for="i in $d.items[0]" :key="i" between hoverable icon>
+          <span>{{ i }}</span>
+          <span>{{ $r(1, 10) }}</span>
+        </q-text>
+      </q-grid>
+      <q-banner :th="['道具说明']" />
+      <q-text>道具说明...</q-text>
+    </template>
+
+    <!-- 重要道具 -->
+    <template v-if="$store.getters.active === 'important'">
+      <q-text>重要道具</q-text>
+      <q-banner :th="['名称', '持有数']" />
+      <q-grid :grid="[10, 1]">
+        <q-text between hoverable icon>
+          <span>万能钥匙</span>
+          <span>1</span>
+        </q-text>
+      </q-grid>
+      <q-banner :th="['道具说明']" />
+      <q-text>道具说明...</q-text>
+    </template>
+
+    <!-- 战车名 -->
+    <template v-if="$store.getters.active === 'tank'">
+      <q-text>战车名</q-text>
+      <q-banner :th="['名称', '重量']" />
+      <q-grid :grid="[10, 1]">
+        <q-text v-for="(e, i) in $d.equipments[1]" :key="i" :icon="[i, 1]" between hoverable>
+          <span>{{ e }}</span>
+          <span>{{ $r(5, 15) }}.00t</span>
+        </q-text>
+      </q-grid>
+      <q-banner :th="['装备说明']" />
+      <q-grid :grid="[4, 2]" column>
+        <q-text v-for="(d, i) in $d.dicts[0]" :key="i" between icon>
+          <span>{{ d }}</span>
+          <span>{{ $d.dicts[1][i] }}</span>
+        </q-text>
+      </q-grid>
+    </template>
   </q-window>
 </template>
 
